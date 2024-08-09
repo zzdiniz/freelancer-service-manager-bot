1863eae725023a4561a2c16684b0c4ff56b919e3
zzdiniz <bruno7240@gmail.com>
2024-08-08 22:03:38 -0300

    feat: create new bot instance base on received id

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,30 @@
-//index
+import express, { Request, Response } from "express"
+import TelegramBot from "node-telegram-bot-api"
+import Message from "./types/Message"
+import Bot from "./types/Bot"
+
+const app = express()
+const PORT = 5000
+const PUBLIC_URL = "https://ferrarezzo.loca.lt/"
+const BOT_TOKEN = "7205792626:AAG6abk_NdmONtDjefWIyiv3r0i70GyWDyg"
+
+const bot = new TelegramBot(BOT_TOKEN, { webHook: true });
+//bot.setWebHook(`${PUBLIC_URL}/webhook/4`);
+//lt --port 5000 --subdomain ferrarezzo
+app.use(express.json());
+
+app.post('/webhook/:id',async (req:Request,res:Response)=>{
+    const {id} = req.params
+    const {message} = req.body
+    console.log('teste',message)
+    const response = await fetch(`http://localhost:3000/bot/get-by-id/${id}`);
+    const botResponse:Bot = await response.json()
+
+    const bot = new TelegramBot(botResponse.token)
+    bot.sendMessage(message.chat.id,`Você disse: ${message.text}`)
+    console.log(`data:${botResponse.name}`)
+    return res.status(200).json({message: `Id retornado: ${id}`})
+})
+app.listen(PORT,()=>{
+    console.log(`Runing bot on port: ${PORT}`)
+})
---
+++ src/types/Message.d.ts
@@ -0,0 +1,23 @@
+export default interface Message {
+  update_id: number;
+  message_id: number;
+  from: From;
+  chat: Chat;
+  date: number;
+  text: string;
+}
+
+interface From {
+  id: number;
+  is_bot: boolean;
+  first_name: string;
+  username: string;
+  language_code: string;
+}
+
+interface Chat {
+  id: number;
+  first_name: string;
+  username: string;
+  type: string;
+}
