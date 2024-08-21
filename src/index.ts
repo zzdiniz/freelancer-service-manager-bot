781ad2a5fc51f6f34c366fbb9eec1537e4d5e508
zzdiniz <bruno7240@gmail.com>
2024-08-20 21:01:33 -0300

    feat: add appointment

--- src/index.ts
+++ src/index.ts
@@ -11,23 +11,26 @@ import getConversation from "./services/getConversation";
 import getProviderById from "./services/getProviderById";
 import updateConversation from "./services/updateConversation";
 import getAvailableDates from "./services/getAvailableDates";
+import addAppointment from "./services/addAppointment";
 
 const app = express();
 const PORT = 5000;
-const ngrok_url = "https://3b27-2804-14c-4e2-42d4-8da-1706-67f3-9f6b.ngrok-free.app";
-const almirtoken = "7315270892:AAEEX-DjOIIIssVfn1-QPYyhV7729YelfeU";
-
+//const ngrok_url = "https://483a-2804-14c-4e2-42d4-fae3-9295-218b-62e5.ngrok-free.app";
+//const ferrarezzo_url = "https://ferrarezzo.loca.lt"
+//const almirtoken = "7315270892:AAEEX-DjOIIIssVfn1-QPYyhV7729YelfeU";
+//const newbot = new TelegramBot(almirtoken)
+//newbot.setWebHook(`${ngrok_url}/webhook/3`)
 app.use(express.json());
 
 app.post("/webhook/:id", async (req: Request, res: Response) => {
-  console.log('Webhook received');
+  console.log("Webhook received");
   const { id } = req.params;
   const { message, callback_query } = req.body as {
     message?: Message;
     callback_query?: CallbackQuery;
   };
 
-  console.log('Request Data:', { message, callback_query });
+  console.log("Request Data:", { message, callback_query });
 
   if (!id) {
     return res.status(422).json({ message: "Id not provided" });
@@ -50,8 +53,12 @@ app.post("/webhook/:id", async (req: Request, res: Response) => {
   const provider = await getProviderById(providerId);
 
   if (!client) {
-    const name = message?.from?.first_name ?? callback_query?.message?.chat.first_name as string;
-    const username = message?.from?.username ?? callback_query?.message?.chat.username as string;
+    const name =
+      message?.from?.first_name ??
+      (callback_query?.message?.chat.first_name as string);
+    const username =
+      message?.from?.username ??
+      (callback_query?.message?.chat.username as string);
 
     await addClient({ id: clientId, name, username });
   }
@@ -59,7 +66,11 @@ app.post("/webhook/:id", async (req: Request, res: Response) => {
   let conversation = await getConversation(providerId, clientId);
 
   if (!conversation) {
-    await addConversation({ providerId, clientId, conversationState: "initial_message" });
+    await addConversation({
+      providerId,
+      clientId,
+      conversationState: "initial_message",
+    });
     conversation = await getConversation(providerId, clientId);
   }
 
@@ -94,25 +105,82 @@ app.post("/webhook/:id", async (req: Request, res: Response) => {
     return res.status(200).json({ message: "Message sent" });
   }
 
-  if (conversation.conversationState === 'service_selection') {
+  if (conversation.conversationState === "service_selection") {
     if (!callback_query) {
-      await bot.sendMessage(chatId, 'Por favor selecione um serviço para que possamos continuar', options);
+      await bot.sendMessage(
+        chatId,
+        "Por favor selecione um serviço para que possamos continuar",
+        options
+      );
       return res.status(200).json({ message: "Message sent" });
     }
 
     const callbackData = callback_query.data;
 
     // Verifique se a callbackData é válida e processe-a
-    if (callbackData?.startsWith('service_')) {
-      const serviceId = callbackData.split('_')[1];
+    if (callbackData?.startsWith("service_")) {
+      const serviceId = callbackData.split("_")[1];
+      console.log(callbackData);
       // Você pode buscar detalhes do serviço ou prosseguir com a lógica desejada
+      console.log("providerId", providerId);
       const availableDates = await getAvailableDates(providerId);
-      console.log('available dates',availableDates)
-      await bot.sendMessage(chatId, `Dates available: ${availableDates[0]}`);
+      const dateOptions = availableDates.map((date) => [
+        {
+          text: `${date.split(" ")[0].split("-").join("/")} às ${
+            date.split(" ")[1]
+          }`,
+          callback_data: JSON.stringify({ date, serviceId }),
+        },
+      ]);
+      const limitedOptions = dateOptions.slice(0, 5);
+      const options = {
+        reply_markup: {
+          inline_keyboard: limitedOptions,
+        },
+      };
+
+      console.log("available dates", availableDates);
+      await updateConversation({
+        providerId,
+        clientId,
+        conversationState: "date_selection",
+      });
+      await bot.sendMessage(
+        chatId,
+        `Perfeito! Agora por favor selecione a uma data e horário detre as opções a seguir:`,
+        options
+      );
       return res.status(200).json({ message: "Dates sent" });
-    } else {
-      await bot.sendMessage(chatId, 'Seleção inválida. Por favor, escolha um serviço da lista.', options);
-      return res.status(400).json({ message: "Invalid selection" });
+    }
+  }
+
+  if (
+    conversation.conversationState === "date_selection" &&
+    callback_query?.data
+  ) {
+    try {
+      const appointment = JSON.parse(callback_query?.data);
+      const defaultdate = (appointment.date as string).split(' ')[0].split('-')
+      console.log('defaultDate',defaultdate)
+      await addAppointment({
+        datetime: `${defaultdate[2]}/${defaultdate[1]}/${defaultdate[0]} ${appointment.date.split(" ")[1]}`,
+        providerId,
+        serviceId: parseInt(appointment.serviceId),
+        status: "scheduled",
+        clientId,
+      });
+      /*await updateConversation({
+        providerId,
+        clientId,
+        conversationState: "service_request",
+      });*/
+      console.log("appointment", appointment);
+    } catch (error) {
+      console.error("Error parsing callback_query.data", error);
+      await bot.sendMessage(
+        chatId,
+        "Erro ao processar sua seleção. Tente novamente."
+      );
     }
   }
 
---
+++ src/services/addAppointment.ts
@@ -0,0 +1,20 @@
+import Appointment from "../types/Appointment";
+
+const addAppointment = async ({ datetime,providerId,serviceId,clientId,status}: Appointment): Promise<void> => {
+    console.log('datetime no fetch',datetime)
+  const response = await fetch(`http://localhost:3000/appointment/add`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ datetime,providerId,serviceId,clientId,status}),
+  });
+
+  if (!response.ok) {
+    throw new Error(`Failed to add appointment: ${response.statusText}`);
+  }
+
+  return;
+};
+
+export default addAppointment;
